e522be06cb59de6d4c55b6b3b8e13b5075408559
Phan Nguyen Khanh Minh <phannguyenkhanhminh@gmail.com>
2025-02-15 16:38:03 +0700

    Hoàn tất giao diện cơ bản (Mới giao diện thôi còn chức năng thì chưa biết có ổn không)

---
+++ DragDropView.swift
@@ -0,0 +1,18 @@
+//
+//  DragDropView.swift
+//  Protect_Environment
+//
+//  Created by Phan Nguyễn Khánh Minh on 15/2/25.
+//
+
+import SwiftUI
+
+struct DragDropView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragDropView()
+}
