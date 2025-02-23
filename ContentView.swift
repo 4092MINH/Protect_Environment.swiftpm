f2afe13d4a1b3cc321a3aa23daf7385ee7191cf0
Phan Nguyen Khanh Minh <phannguyenkhanhminh@gmail.com>
2025-02-24 02:39:44 +0700

    one more

--- ContentView.swift
+++ ContentView.swift
@@ -19,59 +19,109 @@ struct ContentView: View {
 }
 
 struct MinigamesView: View {
+    @State private var showInfo: Int = 0
     @StateObject var wonData = WonGameObservant()
     var body: some View {
-        VStack{
-            NavigationView {
-                VStack {
-                    Text("The Magical Map of Vietnam").font(.title).padding()
-                    Text("Tap to travel and help!")
-                    Spacer()
-                    HStack {
-                        VStack {
-                            if !wonData.wonGame1{
-                                NavigationLink { // UINavigationController.push
-                                    SaveTheOceanView()
-                                        .environmentObject(wonData)
-                                } label: {
+        ZStack {
+            VStack{
+                NavigationView {
+                    VStack {
+                        Text("The Magical Map of Vietnam").font(.title).padding()
+                        Text("Tap to travel and help!")
+                        Spacer()
+                        HStack {
+                            VStack {
+                                if !wonData.wonGame1{
+                                    NavigationLink { // UINavigationController.push
+                                        SaveTheOceanView()
+                                            .environmentObject(wonData)
+                                    } label: {
+                                        VStack {
+                                            Rectangle()
+                                                .fill(Color.white)
+                                                .frame(width: 100, height: 100)
+                                                .overlay(
+                                                    Image("ha-long-bay-menu")
+                                                        .resizable()
+                                                        .scaledToFill()
+                                                )
+                                            Text("Ha Long Bay")
+                                        }
+                                        .padding(.trailing, 30)
+                                    }
+                                } else{
                                     VStack {
                                         Rectangle()
                                             .fill(Color.white)
                                             .frame(width: 100, height: 100)
                                             .overlay(
-                                                Image("ha-long-bay-menu")
+                                                Image("trophy")
                                                     .resizable()
                                                     .scaledToFill()
                                             )
                                         Text("Ha Long Bay")
+                                            .foregroundColor(.orange)
                                     }
                                     .padding(.trailing, 30)
                                 }
-                            } else{
-                                VStack {
-                                    Rectangle()
-                                        .fill(Color.white)
-                                        .frame(width: 100, height: 100)
-                                        .overlay(
-                                            Image("trophy")
-                                                .resizable()
-                                                .scaledToFill()
-                                        )
-                                    Text("Ha Long Bay")
-                                        .foregroundColor(.orange)
+                                Button("info"){
+                                    showInfo = 1
                                 }
                                 .padding(.trailing, 30)
+                                .foregroundColor(.black)
                             }
-                            Button("info"){
-                                print("Ha Long")
+                            VStack {
+                                if !wonData.wonGame2{
+                                    NavigationLink {
+                                        EcoTourismProctectionView()
+                                            .environmentObject(wonData)
+                                    } label: {
+                                        VStack {
+                                            VStack {
+                                                Rectangle()
+                                                    .fill(Color.white)
+                                                    .frame(width: 100, height: 100)
+                                                    .overlay(
+                                                        Image("sapa-menu")
+                                                            .resizable()
+                                                            .scaledToFill()
+                                                    )
+                                                Text("Sapa")
+                                            }.rotationEffect(.degrees(-10))
+                                                .padding(.leading, 20)
+                                        }.rotationEffect(.degrees(10))
+                                            .padding(.leading, 20)
+                                    }
+                                } else {
+                                    VStack {
+                                        VStack {
+                                            Rectangle()
+                                                .fill(Color.white)
+                                                .frame(width: 100, height: 100)
+                                                .overlay(
+                                                    Image("trophy")
+                                                        .resizable()
+                                                        .scaledToFill()
+                                                )
+                                            Text("Sapa")
+                                                .foregroundColor(.orange)
+                                        }.rotationEffect(.degrees(-10))
+                                            .padding(.leading, 20)
+                                    }.rotationEffect(.degrees(10))
+                                        .padding(.leading, 20)
+                                }
+                                
+                                Button("info"){
+                                    showInfo = 2
+                                }
+                                .padding(.leading, 40)
+                                .foregroundColor(.black)
                             }
-                            .padding(.trailing, 30)
-                            .foregroundColor(.black)
-                        }
+                        }.padding(.bottom, 0)
                         VStack {
-                            if !wonData.wonGame2{
+                            if !wonData.wonGame3{
                                 NavigationLink {
-                                    EcoTourismProctectionView()
+                                    DragDropView()
                                         .environmentObject(wonData)
                                 } label: {
                                     VStack {
@@ -80,17 +130,17 @@ struct MinigamesView: View {
                                                 .fill(Color.white)
                                                 .frame(width: 100, height: 100)
                                                 .overlay(
-                                                    Image("sapa-menu")
+                                                    Image("vung-tau-menu")
                                                         .resizable()
                                                         .scaledToFill()
                                                 )
-                                            Text("Sapa")
+                                            Text("Vung Tau")
                                         }.rotationEffect(.degrees(-10))
-                                            .padding(.leading, 20)
+                                            .padding(.top, 0)
                                     }.rotationEffect(.degrees(10))
-                                        .padding(.leading, 20)
+                                        .padding(.top, 0)
                                 }
-                            } else {
+                            } else{
                                 VStack {
                                     VStack {
                                         Rectangle()
@@ -101,91 +151,69 @@ struct MinigamesView: View {
                                                     .resizable()
                                                     .scaledToFill()
                                             )
-                                        Text("Sapa")
-                                            .foregroundColor(.orange)
-                                    }.rotationEffect(.degrees(-10))
-                                        .padding(.leading, 20)
-                                }.rotationEffect(.degrees(10))
-                                    .padding(.leading, 20)
-                            }
-                            
-                            Button("info"){
-                                print("sapa")
-                            }
-                            .padding(.leading, 40)
-                            .foregroundColor(.black)
-                        }
-                    }.padding(.bottom, 0)
-                    VStack {
-                        if !wonData.wonGame3{
-                            NavigationLink {
-                                DragDropView()
-                                    .environmentObject(wonData)
-                            } label: {
-                                VStack {
-                                    VStack {
-                                        Rectangle()
-                                            .fill(Color.white)
-                                            .frame(width: 100, height: 100)
-                                            .overlay(
-                                                Image("vung-tau-menu")
-                                                    .resizable()
-                                                    .scaledToFill()
-                                            )
                                         Text("Vung Tau")
+                                            .foregroundColor(Color.orange)
                                     }.rotationEffect(.degrees(-10))
                                         .padding(.top, 0)
                                 }.rotationEffect(.degrees(10))
                                     .padding(.top, 0)
                             }
-                        } else{
-                            VStack {
-                                VStack {
-                                    Rectangle()
-                                        .fill(Color.white)
-                                        .frame(width: 100, height: 100)
-                                        .overlay(
-                                            Image("trophy")
-                                                .resizable()
-                                                .scaledToFill()
-                                        )
-                                    Text("Vung Tau")
-                                        .foregroundColor(Color.orange)
-                                }.rotationEffect(.degrees(-10))
-                                    .padding(.top, 0)
-                            }.rotationEffect(.degrees(10))
-                                .padding(.top, 0)
+                            Button("info"){
+                                showInfo = 3
+                            }
+                            .padding(.top, 0)
+                            .foregroundColor(.black)
                         }
-                        Button("info"){
-                            print("Vung Tau")
-                        }
-                        .padding(.top, 0)
-                        .foregroundColor(.black)
+                        Spacer()
                     }
-                    Spacer()
+                }
+                if wonData.wonGame1 == true && wonData.wonGame2 == true && wonData.wonGame3 == true {
+                    NavigationLink{
+                        @EnvironmentObject var wonData: WonGameObservant
+                        if wonData.secretEnding{
+                            SecretEndingView()
+                        }
+                        else{
+                            NormalEndingView()
+                        }
+                    } label:{
+                        Image("arrow")
+                            .resizable()
+                            .frame(width: 200, height: 100)
+                            .scaledToFit()
+                            .padding(.bottom, 5)
+                            .overlay(
+                                Text("Come back to Minh's house")
+                                    .font(.custom("chalkduster", size: 20))
+                                    .foregroundColor(.white)
+                            )
+                    }
                 }
             }
-            if wonData.wonGame1 == true && wonData.wonGame2 == true && wonData.wonGame3 == true {
-                NavigationLink{
-                    @EnvironmentObject var wonData: WonGameObservant
-                    if wonData.secretEnding{
-                        SecretEndingView()
-                    }
-                    else{
-                        NormalEndingView()
-                    }
-                } label:{
-                    Image("arrow")
-                        .resizable()
-                        .frame(width: 200, height: 100)
-                        .scaledToFit()
-                        .padding(.bottom, 5)
-                        .overlay(
-                            Text("Come back to Minh's house")
+            if (showInfo != 0){
+                Rectangle()
+                    .frame(width: 350, height: 600)
+                    .cornerRadius(10)
+                    .foregroundColor(.gray)
+                    .overlay(
+                        VStack{
+                            Spacer()
+                            Text(infoPlace[showInfo]) // Hoặc Text bạn muốn hiển thị
                                 .font(.custom("chalkduster", size: 20))
                                 .foregroundColor(.white)
-                        )
-                }
+                            Spacer()
+                            Button("Close"){
+                                showInfo = 0
+                            }
+                            .padding(.bottom, 10)
+                            .padding(.top, 10)
+                            .padding(.horizontal, 20)
+                            .background(Color.red)
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                            Spacer()
+                        }
+                    )
             }
         }
     }
--- InfoData.swift
+++ InfoData.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+var infoPlace: [String] = [
+    "",
+    "Plastic waste, sewage, noise, and over-tourism are polluting Ha Long Bay. Its diverse marine ecosystem, natural beauty, and the tourism industry are negatively impacted. Environmental protection efforts are underway, but stronger action is needed. The future of this World Heritage Site depends on it.",
+    "Some tourists in Sapa still litter, disrespect local customs, haggle excessively, or engage in unsustainable tourism practices. This negatively impacts the environment, culture, and Sapa's tourism image. However, many tourists demonstrate responsible behavior and contribute positively. Raising overall awareness remains a long-term challenge.",
+    "Vung Tau, with its busy port and oil and gas activities, faces the risk of oil spills polluting the marine environment. These spills, whether large or small, harm the ecosystem, impact the livelihoods of fishermen, and damage the tourism landscape. Oil spill response is complex, costly, and requires multi-agency coordination. Raising environmental awareness and investing in prevention and response technologies are crucial for protecting Vung Tau's waters."
+
+]
